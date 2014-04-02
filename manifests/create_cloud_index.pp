d8177e3bfd39e7a45fdd7f76a85d420592a49601
root on puppetmaster <root@puppetmaster.vagrant.local>
2014-04-02 14:16:21 +0000

    separata creazione delle conf su zookeeper dalla creazione degli indici

--- manifests/create_cloud_index.pp
+++ manifests/create_cloud_index.pp
@@ -39,17 +39,25 @@ define solr::create_cloud_index (
     }
   }
 
-  exec {"upload_zookeeper_conf_${index}":
-    command => "${solr_root}/solr-${solr_version}-scripts/zkcli.sh -z $zookeeper_addr -cmd upconfig -confdir /usr/local/etc/solr_xml_config/${index_type} -confname ${index}",
-    unless  => "${solr_root}/solr-${solr_version}-scripts/zkcli.sh -z $zookeeper_addr/configs/${index} -cmd list 2> /dev/null",
+  if !defined(Exec["update_zookeeper_conf_${index_type}"]) {
+    exec {"update_zookeeper_conf_${index_type}":
+      command     => "${solr_root}/solr-${solr_version}-scripts/zkcli.sh -z $zookeeper_addr -cmd upconfig -confdir /usr/local/etc/solr_xml_config/${index_type} -confname ${index_type}",
+      refreshonly => true,
+      subscribe   => File["/usr/local/etc/solr_xml_config/${index_type}"]
+    }
+  }
+
+  exec {"upload_zookeeper_conf_${index_type}_${index}":
+    command => "${solr_root}/solr-${solr_version}-scripts/zkcli.sh -z $zookeeper_addr -cmd upconfig -confdir /usr/local/etc/solr_xml_config/${index_type} -confname ${index_type}",
+    unless  => "${solr_root}/solr-${solr_version}-scripts/zkcli.sh -z $zookeeper_addr/configs/${index_type} -cmd list 2> /dev/null",
     require => File["/usr/local/etc/solr_xml_config/${index_type}"]
   }
 
   exec {"create_collection_${index}":
     # la curl prende lo stato di uscita della chiamata. Se 200 OK
-    command => "/usr/bin/curl -IL -w \"%{response_code}\" 'http://${address}:${port}/solr/admin/collections?action=CREATE&name=${index}&numShards=${num_shards}&replicationFactor=${replication_factor}&collection.configName=${index}' -o /dev/null -q | grep 200",
+    command => "/usr/bin/curl -IL -w \"%{response_code}\" 'http://${address}:${port}/solr/admin/collections?action=CREATE&name=${index}&numShards=${num_shards}&replicationFactor=${replication_factor}&collection.configName=${index_type}' -o /dev/null -q | grep 200",
     unless  => "${solr_root}/solr-${solr_version}-scripts/zkcli.sh -z $zookeeper_addr/collections/${index} -cmd list 2> /dev/null",
-    require => Exec["upload_zookeeper_conf_${index}"]
+    require => Exec["upload_zookeeper_conf_${index_type}"]
   }
 
 }
