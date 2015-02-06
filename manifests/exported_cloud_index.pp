dc4e59eb9bf40194d7e432b00890f89618063521
Lorenzo Cocchi <lorenzo.cocchi@softecspa.it>
2015-02-06 11:10:25 +0100

    fix

--- manifests/exported_cloud_index.pp
+++ manifests/exported_cloud_index.pp
@@ -9,22 +9,22 @@ define solr::exported_cloud_index (
   $solr_root,
   $solr_version,
   $index_type,
-  solr_balanced,
+  $solr_balanced,
   $zookeeper_balanced,
 ) {
 
   if !defined(Solr::Create_cloud_index[$index]) {
     solr::create_cloud_index{$index:
-      num_shards          => $num_shards,
-      replication_factor  => $replication_factor,
-      address             => $address,
-      port                => $port,
-      zookeeper_ensemble  => $zookeeper_ensemble,
-      solr_root           => $solr_root,
-      solr_version        => $solr_version,
-      index_type          => $index_type,
-      solr_balanced       => $solr_balanced,
-      zookeeper_balanced  => $zookeeper_balanced
+      num_shards         => $num_shards,
+      replication_factor => $replication_factor,
+      address            => $address,
+      port               => $port,
+      zookeeper_ensemble => $zookeeper_ensemble,
+      solr_root          => $solr_root,
+      solr_version       => $solr_version,
+      index_type         => $index_type,
+      solr_balanced      => $solr_balanced,
+      zookeeper_balanced => $zookeeper_balanced
     }
   }
 
